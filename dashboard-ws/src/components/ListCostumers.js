98820c106a7e0d4ebf40b2ac26521474235d37de
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2022-05-01 23:22:33 +0700

    Add: customers dan employee

---
+++ dashboard-ws/src/components/ListCostumers.js
@@ -0,0 +1,279 @@
+import SearchBox from "./SearchBox";
+import {
+	MdOutlineMoreVert,
+	MdModeEditOutline,
+	MdDeleteOutline,
+	MdAdd,
+} from "react-icons/md";
+
+const ListCostumers = () => {
+	return (
+		<>
+			<div className="bg-white p-5 rounded-lg flex flex-col flex-1 font-nunito-sans">
+				<div className="flex justify-between">
+					<SearchBox placeholder="Search" />
+					<button className="p-4 rounded-lg hover:bg-gray-300">
+						<MdOutlineMoreVert />
+					</button>
+				</div>
+				<div className="flex flex-row flex-wrap items-center mt-5 space-x-2">
+					<button
+						type="submit"
+						className="h-10 px-2 bg-white border-2 border-black text-black hover:bg-black hover:text-white rounded-lg flex items-center"
+					>
+						Today
+					</button>
+					<input
+						className="border-2 border-black bg-white h-10 px-2 rounded-lg text-sm focus:outline-none"
+						type="date"
+						name="dateTo"
+						id="dateTo"
+					/>
+					<div className="flex flex-1 justify-end">
+						<button
+							type="submit"
+							className="px-3 py-2 bg-black rounded-lg flex items-center"
+						>
+							<MdAdd className="text-white mr-2"/>
+							<span className="text-white">Add Member</span>
+						</button>
+					</div>		
+				</div>
+				<table className="mt-5 font-nunito-sans text-xs w-full overflow-y-scroll">
+					<thead>
+						<tr className="bg-[#F9F9FC] text-black text-left">
+							<th className="py-2">Member ID</th>
+							<th className="py-2">Birthday</th>
+							<th className="py-2">Name</th>
+							<th className="py-2">Membership</th>
+							<th className="py-2">Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">VIP</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+						<tr className="even:bg-[#F9F9FC]">
+							<td className="py-2">#M081234567892</td>
+							<td className="py-2">03-Feb-1990</td>
+							<td className="py-2">Elis Tya Suitu</td>
+							<td className="py-2">Regular</td>
+							<td className="py-2">
+								<button>
+									<MdModeEditOutline className="text-red-500 hover:text-red-800" />
+								</button>
+								<button>
+									<MdDeleteOutline className="text-red-500 hover:text-red-800" />
+								</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<nav
+					class="flex flex-row space-x-3 mx-auto"
+					aria-label="Pagination"
+				>
+					<a
+						href="/presensi"
+						aria-current="page"
+						className="z-10 bg-black border-black text-white relative inline-flex items-center px-4 py-2 border text-sm font-medium rounded-lg"
+					>
+						{" "}
+						1{" "}
+					</a>
+					<a
+						href="/presensi"
+						className="bg-white border-black text-black hover:bg-gray-900 hover:text-gray-300 relative inline-flex items-center px-4 py-2 border text-sm font-medium rounded-lg"
+					>
+						{" "}
+						2{" "}
+					</a>
+					<a
+						href="/presensi"
+						className="bg-white border-black text-black hover:bg-gray-900 hover:text-gray-300 relative inline-flex items-center px-4 py-2 border text-sm font-medium rounded-lg"
+					>
+						{" "}
+						3{" "}
+					</a>
+					<a
+						href="/presensi"
+						className="relative inline-flex items-center px-2 py-2 rounded-lg border border-black bg-black text-sm font-medium text-white hover:bg-gray-900 hover:text-gray-300"
+					>
+						<span class="sr-only">Previous</span>
+						<svg
+							class="h-5 w-5"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</a>
+					<a
+						href="/presensi"
+						className="relative inline-flex items-center px-2 py-2 rounded-lg border border-black bg-black text-sm font-medium text-white hover:bg-gray-900 hover:text-gray-300"
+					>
+						<span class="sr-only">Next</span>
+						<svg
+							class="h-5 w-5"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</a>
+				</nav>
+			</div>
+		</>
+	);
+};
+
+export default ListCostumers;
--- dashboard-ws/src/pages/Employee.js
+++ dashboard-ws/src/pages/Employee.js
@@ -1,6 +1,24 @@
+import DetailEmployee from "components/DetailEmployee";
+import ListEmployee from "components/ListEmployee";
+
 export default function Employee() {
-  return (
-    <>
-    </>
-  );
+	return (
+		<>
+			<div class="flex flex-row">
+				<button class="bg-white text-black rounded-lg active:outline-none ml-3 my-2 px-3 py-2 font-noto-sans font-semibold">
+					All
+				</button>
+				<button class="bg-black text-white rounded-lg hover:bg-white hover:text-black active:outline-none ml-3 my-2 px-3 py-2 font-noto-sans font-semibold">
+					Senior
+				</button>
+				<button class="bg-black text-white rounded-lg hover:bg-white hover:text-black active:outline-none ml-3 my-2 px-3 py-2 font-noto-sans font-semibold">
+					Junior
+				</button>
+			</div>
+			<div className="flex flex-row space-x-5 ml-2 max-h-screen overflow-hidden">
+				<ListEmployee />
+        <DetailEmployee />
+			</div>
+		</>
+	);
 }
