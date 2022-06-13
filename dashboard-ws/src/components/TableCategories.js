e2f06d5f0b65f20bf4ad8ee83c36cb0e7819fb7c
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2022-06-13 12:58:17 +0700

    Feat: method post and get in categories
    Signed-off-by: Hasymi Al <68429761+zxyvyx@users.noreply.github.com>

---
+++ dashboard-ws/src/components/TableCategories.js
@@ -0,0 +1,89 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Pagination from "./Pagination";
+
+const TableCategories = () => {
+	const [tableData, setTableData] = useState([]);
+	const [tableCount, setTableCount] = useState(null);
+	const [currentTablePage, setTablePage] = useState(1);
+	const [itemsPerPage, setItemsPerPage] = useState(1);
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const fetchData = async page => {
+		try {
+			const pageData = await axios.get(
+				`https://katto.osorateam.com/api/products?page=${page}`
+			);
+			setTableData(pageData.data.data);
+		} catch (err) {
+			console.log("error in fetching table data", err);
+		}
+	};
+
+	useEffect(() => {
+		const getTotalCount = async () => {
+			try {
+				const AllData = await axios.get(
+					"https://katto.osorateam.com/api/products"
+				);
+				setTableCount(AllData.data.meta.total);
+			} catch (err) {
+				console.log("error in fetching table data", err);
+			}
+		};
+		getTotalCount();
+	}, []);
+
+	useEffect(() => {
+		const getItemsPerPage = async () => {
+			try {
+				const CountPerPage = await axios.get(
+					"https://katto.osorateam.com/api/products"
+				);
+				setItemsPerPage(CountPerPage.data.meta.per_page);
+			} catch (err) {
+				console.log("error in fetching table data", err);
+			}
+		};
+		getItemsPerPage();
+	}, []);
+
+	const showTablePage = page => {
+		setTablePage(page);
+		fetchData(page);
+	};
+
+	return (
+		<>
+			<table className="mt-5 font-nunito-sans text-xs w-full overflow-y-scroll relative">
+				<thead className="sticky top-0">
+					<tr className="bg-[#F9F9FC] text-black text-left">
+						<th className="py-2">Id</th>
+						<th className="py-2">Name</th>
+					</tr>
+				</thead>
+				<tbody>
+					{tableData.length > 0 &&
+						tableData.map(row => (
+							<tr
+								key={row.id}
+								className="even:bg-[#F9F9FC] text-black"
+							>
+								<td className="py-2">{row.code}</td>
+								<td className="py-2">{row.name}</td>
+							</tr>
+						))}
+				</tbody>
+			</table>
+			{tableCount && (
+				<Pagination
+					maxPage={Math.ceil(tableCount / itemsPerPage)}
+					currentPage={currentTablePage}
+					showTablePage={showTablePage}
+				/>
+			)}
+		</>
+	);
+};
+
+export default TableCategories;
