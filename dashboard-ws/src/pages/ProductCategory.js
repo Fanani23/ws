90028c37b51559db2051976038ec71059bf19722
iMoody <99703360+Fanani23@users.noreply.github.com>
2022-06-14 12:32:10 +0700

    Update ProductCategory.js

--- dashboard-ws/src/pages/ProductCategory.js
+++ dashboard-ws/src/pages/ProductCategory.js
@@ -106,7 +106,7 @@ const ProductCategory = () => {
 											<button
 												type="submit"
 												className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2 mr-4"
-												onClick={closeAddCategoryModal}
+												
 											>
 												Save
 											</button>
