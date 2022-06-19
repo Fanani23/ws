0c1252a23c9e2aa467d1ffd4848ab7fc64b7904c
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2022-06-20 03:30:08 +0700

    Update: Categories
    Signed-off-by: Hasymi Al <68429761+zxyvyx@users.noreply.github.com>

--- dashboard-ws/src/pages/ProductCategory.js
+++ dashboard-ws/src/pages/ProductCategory.js
@@ -1,161 +1,241 @@
-import {
-	MdSearch,
-	MdAdd,
-	MdModeEditOutline,
-	MdDeleteOutline,
-	MdClose,
-} from "react-icons/md";
-import {Fragment, useState} from "react";
-import {Link} from "react-router-dom";
+import {MdAdd} from "react-icons/md";
+import {Fragment, useEffect, useState} from "react";
 import {Dialog, Transition} from "@headlessui/react";
 import TabTitle from "../utils/GeneralFunction";
 import TableCategories from "../components/TableCategories";
+import axios from "axios";
+import Search from "../components/Search";
+import Pagination from "../components/Pagination";
+import ModalCreateCategories from "../components/ModalCreateCategories";
+import ModalEditCategories from "../components/ModalEditCategories";
+import ModalDeleteCategories from "../components/ModalDeleteCategories";
 
 const ProductCategory = () => {
-	TabTitle("Category - Kato Haircut");
-	const [openAddCategory, setOpenAddCategory] = useState(false);
+  TabTitle("Category - Kato Haircut");
+  // modal
+  const [openAddCategory, setOpenAddCategory] = useState(false);
+  const closeAddCategoryModal = () => setOpenAddCategory(false);
+  const openAddCategoryModal = () => setOpenAddCategory(true);
+  const [openEditCategory, setOpenEditCategory] = useState(false);
+  const closeEditCategoryModal = () => setOpenEditCategory(false);
+  const [openDeleteCategory, setOpenDeleteCategory] = useState(false);
+  const closeDeleteCategoryModal = () => setOpenDeleteCategory(false);
+  // table and pagination
+  const [tableData, setTableData] = useState([]);
+  const [tableCount, setTableCount] = useState(null);
+  const [currentTablePage, setCurrentTablePage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(1);
+  // search
+  const [searchValue, setSearchValue] = useState();
+  // handle create
+  const [code, setCode] = useState("");
+  const [name, setName] = useState("");
+  // handle edit
+  const [idEdit, setIdEdit] = useState("");
+  const [codeEdit, setCodeEdit] = useState("");
+  const [nameEdit, setNameEdit] = useState("");
+  // handle delete
+  const [idDelete, setIdDelete] = useState("");
+  const [nameDelete, setNameDelete] = useState("");
 
-	const closeAddCategoryModal = () => {
-		setOpenAddCategory(false);
-	};
+  const fetchData = async (page = currentTablePage, search = "") => {
+    try {
+      const pageData = await axios.get(
+        `https://api.kattohair.com/api/products/categories${
+          search !== "" ? `?name=${search}&?page=${page}` : `?page=${page}`
+        }`
+      );
+      setTableData(pageData.data.data);
+    } catch (err) {
+      console.log("error in fetching table data", err);
+    }
+  };
 
-	const openAddCategoryModal = () => {
-		setOpenAddCategory(true);
-	};
+  const getTotalCount = async (page = currentTablePage, search = "") => {
+    try {
+      const AllData = await axios.get(
+        `https://api.kattohair.com/api/products/categories${
+          search !== "" ? `?name=${search}&?page=${page}` : `?page=${page}`
+        }`
+      );
+      setTableCount(AllData.data.meta.total);
+    } catch (err) {
+      console.log("error in fetching table data", err);
+    }
+  };
 
-	return (
-		<div className="w-full flex flex-col grow overflow-auto scrollbar-shown">
-			<Transition appear show={openAddCategory} as={Fragment}>
-				<Dialog
-					as="div"
-					className="relative z-10"
-					onClose={closeAddCategoryModal}
-				>
-					<Transition.Child
-						as={Fragment}
-						enter="ease-out duration-300"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-						leave="ease-in duration-200"
-						leaveFrom="opacity-100"
-						leaveTo="opacity-0"
-					>
-						<div className="fixed inset-0 bg-black bg-opacity-50" />
-					</Transition.Child>
+  const getItemsPerPage = async (page = currentTablePage, search = "") => {
+    try {
+      const CountPerPage = await axios.get(
+        `https://api.kattohair.com/api/products/categories${
+          search !== "" ? `?name=${search}&?page=${page}` : `?page=${page}`
+        }`
+      );
+      setItemsPerPage(CountPerPage.data.meta.per_page);
+    } catch (err) {
+      console.log("error in fetching table data", err);
+    }
+  };
 
-					<div className="fixed inset-0 overflow-y-auto">
-						<div className="flex min-h-full items-center justify-center text-center">
-							<Transition.Child
-								as={Fragment}
-								enter="ease-out duration-300"
-								enterFrom="opacity-0 scale-95"
-								enterTo="opacity-100 scale-100"
-								leave="ease-in duration-200"
-								leaveFrom="opacity-100 scale-100"
-								leaveTo="opacity-0 scale-95"
-							>
-								<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
-									<Dialog.Title
-										as="div"
-										className="text-lg text-center font-medium leading-6 text-gray-900 p-8 pb-1"
-									>
-										<h3>Add Data Category</h3>
-										<div
-											onClick={closeAddCategoryModal}
-											className="rounded-full p-0.5 top-2 right-2 bg-gray-200 absolute"
-											role="button"
-										>
-											<MdClose className="relative" />
-										</div>
-									</Dialog.Title>
-									<form autoComplete="off" noValidate>
-										<div className="mt-2 border-t-2">
-											<div className="text-sm p-6 text-gray-500">
-												<div className="flex flex-row items-center mb-2">
-													<label
-														htmlFor="id-category"
-														className="font-semibold w-28"
-													>
-														ID Category
-													</label>
-													<input
-														type="text"
-														name="id-category"
-														id="id-category"
-														className="border-2 grow border-gray-200 rounded-lg px-3 py-2"
-													/>
-												</div>
-												<div className="flex flex-row items-center">
-													<label
-														htmlFor="category-name"
-														className="font-semibold w-28"
-													>
-														Category Name
-													</label>
-													<input
-														type="text"
-														name="category-name"
-														id="category-name"
-														className="border-2 grow border-gray-200 rounded-lg px-3 py-2"
-													/>
-												</div>
-											</div>
-										</div>
+  useEffect(() => {
+    fetchData();
+    getTotalCount();
+    getItemsPerPage();
+  }, []);
 
-										<div className="mt-4 px-6 pb-6 flex justify-center">
-											<button
-												type="submit"
-												className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2 mr-4"
-												onClick={closeAddCategoryModal}
-											>
-												Save
-											</button>
-											<button
-												type="button"
-												className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
-												onClick={closeAddCategoryModal}
-											>
-												Cancel
-											</button>
-										</div>
-									</form>
-								</Dialog.Panel>
-							</Transition.Child>
-						</div>
-					</div>
-				</Dialog>
-			</Transition>
-			<div className="bg-white w-full p-5 rounded-lg overflow-hidden flex h-full flex-col">
-				<div className="flex justify-between">
-					<div className="flex grow text-gray-600">
-						<MdSearch className="absolute top-3 left-0 text-white" />
-						<input
-							className="border-b-2 border-gray-300 bg-transparent w-full md:w-1/2 lg:w-1/3 text-white h-10 pl-5 text-sm focus:outline-none"
-							type="search"
-							name="search"
-							id="search"
-							placeholder="Search"
-						/>
-						<button
-							type="submit"
-							className="ml-2 mb-2 px-3 py-2 bg-black rounded-lg"
-						>
-							<MdSearch className="text-white" />
-						</button>
-					</div>
-					<button
-						type="submit"
-						className="flex items-center ml-2 mb-2 px-3 py-2 bg-black rounded-lg"
-						onClick={openAddCategoryModal}
-					>
-						<MdAdd className="text-white mr-2" />
-						<span>Add Category</span>
-					</button>
-				</div>
-				<TableCategories />
-			</div>
-		</div>
-	);
+  const showTablePage = (page) => {
+    setCurrentTablePage(page);
+    fetchData(page);
+  };
+
+  const showSearchedTablePage = (searchValue) => {
+    setSearchValue(searchValue);
+    setCurrentTablePage(1);
+    fetchData(currentTablePage, searchValue);
+    getTotalCount(currentTablePage, searchValue);
+    getItemsPerPage(currentTablePage, searchValue);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        "https://api.kattohair.com/api/products/categories/create",
+        {code, name}
+      );
+      fetchData();
+      getTotalCount();
+      getItemsPerPage();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const prepareEdit = (value) => {
+    setIdEdit(value);
+    getEditData(value);
+    setOpenEditCategory(true);
+  };
+
+  const getEditData = async (value) => {
+    try {
+      const {data} = await axios.get(
+        `https://api.kattohair.com/api/products/categories/${value}}`
+      );
+      setCodeEdit(data.data.code);
+      setNameEdit(data.data.name);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    try {
+      let data = await axios.put(
+        `https://api.kattohair.com/api/products/categories/update/${idEdit}`,
+        {code: codeEdit, name: nameEdit}
+      );
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const prepareDelete = (id) => {
+    setIdDelete(id);
+    console.log(id);
+    getDeleteData(id);
+    setOpenDeleteCategory(true);
+  };
+
+  const getDeleteData = async (id) => {
+    try {
+      const {data} = await axios.get(
+        `https://api.kattohair.com/api/products/categories/${id}`
+      );
+      setNameDelete(data.data.name);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(
+        `https://api.kattohair.com/api/products/categories/delete/${idDelete}`
+      );
+      fetchData();
+      getTotalCount();
+      getItemsPerPage();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="w-full flex flex-col grow overflow-auto scrollbar-shown">
+      <ModalCreateCategories
+        show={openAddCategory}
+        close={closeAddCategoryModal}
+        code={code}
+        setCodeValue={setCode}
+        name={name}
+        setNameValue={setName}
+        submit={handleSubmit}
+      />
+      <ModalEditCategories
+        show={openEditCategory}
+        close={closeEditCategoryModal}
+        codeEditValue={codeEdit}
+        setCodeEditValue={setCodeEdit}
+        nameEditValue={nameEdit}
+        setNameEditValue={setNameEdit}
+        submit={handleEdit}
+      />
+      <ModalDeleteCategories
+        show={openDeleteCategory}
+        close={closeDeleteCategoryModal}
+        nameDeleteValue={nameDelete}
+        submit={handleDelete}
+      />
+      <div className="bg-white w-full p-5 rounded-lg overflow-hidden flex h-full flex-col">
+        <div className="flex justify-between">
+          <Search
+            textColor={"text-black"}
+            bgColor={"bg-white"}
+            placeholder={"Search by name..."}
+            searchValue={searchValue}
+            setSearchValue={showSearchedTablePage}
+          />
+          <button
+            type="submit"
+            className="flex items-center ml-2 mb-2 px-3 py-2 bg-black rounded-lg"
+            onClick={openAddCategoryModal}
+          >
+            <MdAdd className="text-white mr-2" />
+            <span>Add Category</span>
+          </button>
+        </div>
+        {tableCount ? (
+          <>
+            <TableCategories
+              tableData={tableData}
+              editRow={prepareEdit}
+              deleteRow={prepareDelete}
+            />
+            <Pagination
+              maxPage={Math.ceil(tableCount / itemsPerPage)}
+              currentPage={currentTablePage}
+              showTablePage={showTablePage}
+            />
+          </>
+        ) : (
+          <p className="w-full text-black">No result</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProductCategory;
