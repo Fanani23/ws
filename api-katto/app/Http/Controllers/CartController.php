b3e4e70313e48c44d2a98af1c3157818aede9d44
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-06-28 00:51:49 +0700

    vip customer get 20% disc

--- api-katto/app/Http/Controllers/CartController.php
+++ api-katto/app/Http/Controllers/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CartResource;
 use App\Models\Cart;
+use App\Models\Customer;
 use App\Models\Product;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
@@ -16,6 +17,12 @@ class CartController extends Controller
         $subtotal = 0;
         $price_after_discount = [];
 
+        $discount_amount = request()->discount_amount;
+        $coupon_amount = request()->coupon_amount;
+        $discount_type = $request->discount_type;
+
+        $customer_membership = Customer::where('id', $request->customer_id)->first()->membership;
+
         // discount per service
         foreach ($request->service_items as $key => $service) {
             $product = Product::where('id', $service['product_id'])->first();
@@ -23,36 +30,47 @@ class CartController extends Controller
 
             $subtotal += $product_price;
 
-            if ($service['service_discount_type'] == 'nominal') {
-                $price_after_discount[$key] = $product_price - $service['service_discount_amount'];
-            } elseif ($service['service_discount_type'] == 'percent') {
-                $price_after_discount[$key] = $product_price - ($product_price * ($service['service_discount_amount'] / 100));
+            if ($customer_membership == 'vip') {
+                $price_after_discount[$key] = $product_price;
+            } else {
+                if ($service['service_discount_type'] == 'nominal') {
+                    $price_after_discount[$key] = $product_price - $service['service_discount_amount'];
+                } elseif ($service['service_discount_type'] == 'percent') {
+                    $price_after_discount[$key] = $product_price - ($product_price * ($service['service_discount_amount'] / 100));
+                }
             }
         }
 
         $grand_total = array_sum($price_after_discount);
 
-        // discount order
-        if ($request->discount_type == 'nominal') {
-            $grand_total = $grand_total - $request->discount_amount;
-        } elseif ($request->discount_type == 'percent') {
-            $grand_total = $grand_total - ($grand_total * ($request->discount_amount / 100));
-        }
+        if ($customer_membership == 'vip') {
+            $grand_total = $grand_total - ($grand_total * (20 / 100));
+            $discount_amount = 20;
+            $discount_type = 'percent';
+            $coupon_amount = 0;
+        } else {
+            // discount order
+            if ($request->discount_type == 'nominal') {
+                $grand_total = $grand_total - $request->discount_amount;
+            } elseif ($request->discount_type == 'percent') {
+                $grand_total = $grand_total - ($grand_total * ($request->discount_amount / 100));
+            }
 
-        if ($request->coupon_type == 'nominal') {
-            $grand_total = $grand_total - $request->coupon_amount;
-        } elseif ($request->coupon_type == 'percent') {
-            $grand_total = $grand_total - ($grand_total * ($request->coupon_amount / 100));
+            if ($request->coupon_type == 'nominal') {
+                $grand_total = $grand_total - $request->coupon_amount;
+            } elseif ($request->coupon_type == 'percent') {
+                $grand_total = $grand_total - ($grand_total * ($request->coupon_amount / 100));
+            }
         }
 
         $cart = Cart::create([
             'user_id' => $request->user_id,
             'customer_id' => $request->customer_id,
             'code' => time(),
-            'discount_type' => $request->discount_type,
-            'discount_amount' => $request->discount_amount,
+            'discount_type' => $discount_type,
+            'discount_amount' => $discount_amount,
             'coupon_type' => $request->coupon_type,
-            'coupon_amount' => $request->coupon_amount,
+            'coupon_amount' => $coupon_amount,
             'subtotal' => $subtotal,
             'discount_total' => $subtotal - $grand_total,
             'grand_total' => $grand_total,
@@ -63,7 +81,7 @@ class CartController extends Controller
         foreach ($request->service_items as $key => $service) {
             $product = Product::where('id', $service['product_id'])->first();
             $product_price = $product['price'];
-            
+
             $fee = 0;
             if ($product->commission_type == 'percent') {
                 // product price or price after discount
@@ -72,32 +90,39 @@ class CartController extends Controller
                 $fee = $product['commission_value'];
             }
 
-            $service_discount_type = $service['service_discount_type'];
-            $service_discount_amount = $service['service_discount_amount'];
-            $price_after_discountaa = $price_after_discount[$key];
-
-            // discount
-            if ($request->discount_type == 'nominal') {
-                $service_discount_type = 'nominal';
-                $service_discount_amount = $request->discount_amount / $count_services;
-                $price_after_discountaa = $product_price - ($request->discount_amount / $count_services);
-            } elseif ($request->discount_type == 'percent') {
+            if ($customer_membership == 'vip') {
                 $service_discount_type = 'percent';
-                $service_discount_amount = $request->discount_amount;
-                $price_after_discountaa = $product_price - ($product_price * ($request->discount_amount / 100));
-            }
-            
-            // coupon
-            if ($request->coupon_type == 'nominal') {
-                $service_discount_type = 'nominal';
-                $service_discount_amount = $request->coupon_amount / $count_services;
-                $price_after_discountaa = $product_price - ($request->coupon_amount / $count_services);
-            } elseif ($request->coupon_type == 'percent') {
-                $service_discount_type = 'percent';
-                $service_discount_amount = $request->coupon_amount;
-                $price_after_discountaa = $product_price - ($product_price * ($request->coupon_amount / 100));
+                $service_discount_amount = 20;
+                $price_after_discount_product = $product_price - ($product_price * (20 / 100));
+            } else {
+                $service_discount_type = $service['service_discount_type'];
+                $service_discount_amount = $service['service_discount_amount'];
+                $price_after_discount_product = $price_after_discount[$key];
+
+                // discount
+                if ($request->discount_type == 'nominal' && $request->discount_amount > 0) {
+                    $service_discount_type = 'nominal';
+                    $service_discount_amount = $request->discount_amount / $count_services;
+                    $price_after_discount_product = $product_price - ($request->discount_amount / $count_services);
+                } elseif ($request->discount_type == 'percent' && $request->discount_amount > 0) {
+                    $service_discount_type = 'percent';
+                    $service_discount_amount = $request->discount_amount;
+                    $price_after_discount_product = $product_price - ($product_price * ($request->discount_amount / 100));
+                }
+
+                // coupon
+                if ($request->coupon_type == 'nominal' && $request->coupon_amount > 0) {
+                    $service_discount_type = 'nominal';
+                    $service_discount_amount = $request->coupon_amount / $count_services;
+                    $price_after_discount_product = $product_price - ($request->coupon_amount / $count_services);
+                } elseif ($request->coupon_type == 'percent' && $request->coupon_amount > 0) {
+                    $service_discount_type = 'percent';
+                    $service_discount_amount = $request->coupon_amount;
+                    $price_after_discount_product = $product_price - ($product_price * ($request->coupon_amount / 100));
+                }
             }
 
+            // return $service_discount_amount;
 
             $cart->cartItems()->create([
                 'employee_id' => $service['stylist_id'],
@@ -106,7 +131,7 @@ class CartController extends Controller
                 'discount_type' => $service_discount_type,
                 'discount_amount' => $service_discount_amount,
                 'price' => $product_price,
-                'price_after_discount' => $price_after_discountaa,
+                'price_after_discount' => $price_after_discount_product,
                 'commission_type' => $product->commission_type,
                 'commission_value' => $product->commission_value,
                 'fee' => $fee,
