30dfa60e39b4d621efe80f1aeaf5a9bff2403e01
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2022-07-06 22:50:35 +0700

    Fix: multiple route layout for authenticated and unauthenticated login
    Signed-off-by: Hasymi Al <68429761+zxyvyx@users.noreply.github.com>

--- dashboard-ws/src/App.js
+++ dashboard-ws/src/App.js
@@ -19,7 +19,6 @@ import {useEffect, useState} from "react";
 import CashierSingle from "./pages/CashierSingle";
 import Jobs from "./pages/Jobs";
 import ReportCommission from "./pages/ReportCommission";
-import Session from "./Session";
 
 function App() {
   const [sidebar, setSidebar] = useState(false);
@@ -40,84 +39,77 @@ function App() {
     return setSidebar(!sidebar);
   };
 
-  let sidebarTag;
-  let toggleSidebarTag;
-
-  if (window.location.pathname === "/login") {
-    sidebarTag = "";
-    toggleSidebarTag = "";
-  } else {
-    if (!Session()) {
-      window.location = "/login";
-      return <div></div>;
-    }
-    sidebarTag = <Sidebar show={sidebar} />;
-    toggleSidebarTag = <Navbar toggleSidebar={toggleSidebar} />;
-  }
-
   return (
-    <BrowserRouter>
-      {/* <Toaster /> */}
-      <div className="flex flex-col bg-black h-screen">
-        {toggleSidebarTag}
-        <div className="flex flex-row h-full mt-20 overflow-hidden">
-          {sidebarTag}
-          <div className="overflow-y-auto p-2 w-full text-white scrollbar-shown">
-            <Routes>
-              <Route path="/">
-                <Route index element={<Dashboard />} />
-                <Route path="login" element={<Login />} />
-                <Route path="cashier">
-                  <Route index element={<Cashier />} />
-                  <Route path=":transactionId" element={<CashierSingle />} />
-                </Route>
-                <Route path="product">
-                  <Route
-                    path="/product"
-                    element={<Navigate to="/product/category" replace />}
-                  />
-                  <Route path="category" element={<ProductCategory />} />
-                  <Route path="list" element={<ProductList />} />
-                </Route>
-                <Route path="report">
-                  <Route
-                    path="/report"
-                    element={<Navigate to="/report/order" replace />}
-                  />
-                  <Route path="order" element={<ReportOrder />} />
-                  <Route path="transaction" element={<ReportTransaction />} />
-                  <Route path="commission" element={<ReportCommission />} />
-                </Route>
-                <Route path="customers">
-                  <Route index element={<Customers />} />
-                  <Route path=":customerId" element={<CustomerSingle />} />
-                </Route>
-                <Route path="presensi" element={<Presensi />} />
-                <Route path="employee">
-                  <Route
-                    path="/employee"
-                    element={<Navigate to="/employee/list" replace />}
-                  />
-                  <Route path="jobs" element={<Jobs />} />
-                  <Route path="list">
-                    <Route index element={<Employee />} />
-                    <Route path=":employeeId" element={<EmployeeSingle />} />
+    <>
+      {localStorage.getItem("user") && localStorage.getItem("token") ? (
+        <div className="flex flex-col bg-black h-screen">
+          <Navbar toggleSidebar={toggleSidebar} />
+          <div className="flex flex-row h-full mt-20 overflow-hidden">
+            <Sidebar show={sidebar} />
+            <div className="overflow-y-auto p-2 w-full text-white scrollbar-shown">
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Dashboard />} />
+                  <Route path="cashier">
+                    <Route index element={<Cashier />} />
+                    <Route path=":transactionId" element={<CashierSingle />} />
                   </Route>
+                  <Route path="product">
+                    <Route
+                      path="/product"
+                      element={<Navigate to="/product/category" replace />}
+                    />
+                    <Route path="category" element={<ProductCategory />} />
+                    <Route path="list" element={<ProductList />} />
+                  </Route>
+                  <Route path="report">
+                    <Route
+                      path="/report"
+                      element={<Navigate to="/report/order" replace />}
+                    />
+                    <Route path="order" element={<ReportOrder />} />
+                    <Route path="transaction" element={<ReportTransaction />} />
+                    <Route path="commission" element={<ReportCommission />} />
+                  </Route>
+                  <Route path="customers">
+                    <Route index element={<Customers />} />
+                    <Route path=":customerId" element={<CustomerSingle />} />
+                  </Route>
+                  <Route path="presensi" element={<Presensi />} />
+                  <Route path="employee">
+                    <Route
+                      path="/employee"
+                      element={<Navigate to="/employee/list" replace />}
+                    />
+                    <Route path="jobs" element={<Jobs />} />
+                    <Route path="list">
+                      <Route index element={<Employee />} />
+                      <Route path=":employeeId" element={<EmployeeSingle />} />
+                    </Route>
+                  </Route>
+                  <Route path="setting">
+                    <Route
+                      path="/setting"
+                      element={<Navigate to="/setting/admin" replace />}
+                    />
+                    <Route path="admin" element={<SettingAdmin />} />
+                    <Route path="log" element={<SettingLogLogin />} />
+                  </Route>
+                  <Route path="/login" element={<Navigate to="/" replace />} />
                 </Route>
-                <Route path="setting">
-                  <Route
-                    path="/setting"
-                    element={<Navigate to="/setting/admin" replace />}
-                  />
-                  <Route path="admin" element={<SettingAdmin />} />
-                  <Route path="log" element={<SettingLogLogin />} />
-                </Route>
-              </Route>
-            </Routes>
+              </Routes>
+            </div>
           </div>
         </div>
-      </div>
-    </BrowserRouter>
+      ) : (
+        <Routes>
+          <Route path="/">
+            <Route index element={<Login />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+      )}
+    </>
   );
 }
 
