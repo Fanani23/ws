e9dea2a73bafd61fd96d8ef68ac47ac526e64ace
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2022-07-07 19:53:48 +0700

    Feat: changes
    Signed-off-by: Hasymi Al <68429761+zxyvyx@users.noreply.github.com>

--- dashboard-ws/src/components/FilterByDate.js
+++ dashboard-ws/src/components/FilterByDate.js
@@ -6,7 +6,11 @@ const FilterByDate = ({dateStart, setDateStart, dateEnd, setDateEnd}) => {
           type="date"
           name="from"
           id="dateStart"
-          className="ml-0 md:ml-2 h-10 border border-gray-300 text-sm bg-white text-black rounded-lg px-2 outline-none focus:ring-black focus:ring-2"
+          value={dateStart}
+          className={`${
+            dateStart ? "text-black " : "text-transparent "
+          }h-10 border border-gray-300 text-sm bg-white rounded-lg px-2 outline-none focus:ring-black focus:ring-2`}
+          onChange={setDateStart}
         />
       </div>
       <span className="text-black mx-2 mt-2">to</span>
@@ -15,9 +19,11 @@ const FilterByDate = ({dateStart, setDateStart, dateEnd, setDateEnd}) => {
           type="date"
           name="to"
           id="dateEnd"
+          value={dateEnd}
           className={`${
             dateEnd ? "text-black " : "text-transparent "
           }h-10 border border-gray-300 text-sm bg-white rounded-lg px-2 outline-none focus:ring-black focus:ring-2`}
+          onChange={setDateEnd}
         />
         {!dateEnd && (
           <span className="absolute top-2 left-2 text-black">Date End</span>
