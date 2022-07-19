87429ac2f416925b7db4c18ecfca239aada7ca99
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-07-19 11:06:02 +0700

    fix swagger bug

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/storage', function () {
+    Artisan::call('storage:link');
+    return "ok";
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/cache', function () {
+    Artisan::call('cache:clear');
+    return "ok";
+});
+
+Route::get('/key', function () {
+    Artisan::call('key:generate');
+    return "ok";
 });
